030e15c716758f9dfe2fa4ac18926259e751808f
sourov22 <47422289+sourov22@users.noreply.github.com>
2019-02-09 17:04:19 +0600

    Update Assignment_Syntax_Analyzer.py

--- Assignment_Syntax_Analyzer.py
+++ Assignment_Syntax_Analyzer.py
@@ -1,13 +1,8 @@
-# import re
-# for getting more accurate output, converting the input file in one line. :)
 with open('text.txt', 'r+') as f:
     contents = f.read()
 
     split_word = contents.split(' ')
 
-# character_list = list(contents)
-
-# print(character_list)
 
 print(split_word)
 
@@ -18,28 +13,27 @@ others = ["{", "}", ";", ")", "(", "[", "]", ",", ';']
 logical_operator = ["<", ">", "&&", "||", "!=", "=="]
 identifiers = ['a', 'b', 'c', 'd']
 
-# Keyword
+
 print('\n\nKeyWord = ', end='')
 for word in split_word:
     for key in keyword:
         if word == key:
             print(word, end=' ')
 
-# Identifiers
+
 print('\n\nidentifiers = ', end='')
 for word in split_word:
     for key in identifiers:
         if word == key:
             print(word, end=' ')
 
-# value
 print('\n\nvalue = ', end='')
 for word in split_word:
     for key in value:
         if word == key:
             print(word, end=' ')
 
-# others
+
 
 print('\n\nothers = ', end='')
 for word in split_word:
@@ -47,7 +41,6 @@ for word in split_word:
         if word == key:
             print(word, end=' ')
 
-# logical_operator
 
 print('\n\nlogical_operator = ', end='')
 for word in split_word:
@@ -55,7 +48,7 @@ for word in split_word:
         if word == key:
             print(word, end=' ')
 
-# operand
+
 
 print('\n\noperator = ', end='')
 for word in split_word:
